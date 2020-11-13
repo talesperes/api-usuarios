37e49a6523583f791ad1cfcefe724cb8536551c9
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2020-11-13 16:16:04 -0300

    arquivos iniciais

---
+++ api/controllers/users.js
@@ -0,0 +1,65 @@
+module.exports = app => {
+    const usersDB = app.data.users;
+    const controller = {};
+
+    const { users: usersMock } = usersDB;
+  
+    controller.listUsers = (req, res) => res.status(200).json(usersDB);
+
+    controller.getUser = (req, res) => {
+
+        const { cpf } = req.params;
+
+        const foundUser = usersMock.data.find(user => user.cpf == cpf)
+
+        if(foundUser) {
+            res.status(200).json(foundUser);
+        } else {
+            res.status(404).json({
+                message: 'Usuário não encontrado na base.',
+                success: false
+            });
+        }
+
+    }
+
+    controller.addUser = (req, res) => {
+
+        console.log(req.body);
+
+        usersMock.data.push({
+            name: req.body.name,
+            birthDate: req.body.birthDate,
+            phone: req.body.phone,
+            email: req.body.email,
+            cpf: req.body.cpf
+        });
+    
+        res.status(201).json(usersMock);
+    };
+
+    controller.removeUser = (req, res) => {
+
+        
+        const { cpf } = req.params;
+    
+        const foundUserIndex = usersMock.data.findIndex(user => user.cpf === cpf);
+    
+        if (foundUserIndex === -1) {
+          res.status(404).json({
+            message: 'Usuário não encontrado na base.',
+            success: false,
+            users: usersMock,
+          });
+        } else {
+          usersMock.data.splice(foundUserIndex, 1);
+          res.status(200).json({
+            message: 'Usuário encontrado e deletado com sucesso!',
+            success: true,
+            users: usersMock,
+          });
+        }
+      };
+  
+    return controller;
+}
---
+++ api/routes/users.js
@@ -0,0 +1,12 @@
+module.exports = app => {
+  const controller = app.controllers.users;
+  
+  app.route('/api/v1/users')
+    // .get(controller.listUsers)
+    .post(controller.addUser)
+
+  app.route('/api/v1/users/:cpf')
+    .delete(controller.removeUser)
+    .get(controller.getUser)
+
+}
